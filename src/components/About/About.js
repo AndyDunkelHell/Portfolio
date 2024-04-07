94d9b21a918cbcd131dd53a0cc047fc9f64f1cc4
Andres Gonzalez <andres.gonzalez-rivera@alumni.fh-aachen.de>
2024-04-07 17:57:26 +0200

    update about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -30,25 +30,25 @@ const About = () => {
           <div className="mt-5">
             <h1
               ref={textOne}
-              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-4/5 mob:w-full laptop:w-4/5"
+              className="about name"
             >
               {name}
             </h1>
             <h1
               ref={textTwo}
-              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
+              
             >
               {role}
             </h1>
             <h1
               ref={textThree}
-              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
+              
             >
               {role2}
             </h1>
             <h1
               ref={textFour}
-              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
+              
             >
               {role3}
             </h1>
