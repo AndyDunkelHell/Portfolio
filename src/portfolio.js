8e9252d1af443da11b3fc9deb0981350bed60dad
Andres Gonzalez <andres.gonzalez-rivera@alumni.fh-aachen.de>
2024-04-08 00:25:01 +0200

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -14,7 +14,7 @@ const about = {
     'Professional tinkerer',
   description:
     "I am Andres Gonzalez, 24 years old. I'm currently studying a Master's degree in Medical Informatics at the University of Leipzig. I come from Venezuela and am in Germany so that I can make my dream come true, namely to help millions of people in the future with my developments. In the first year of my life in Germany I was in Cologne as an exchange student, then I studied for a year in a Studienkolleg in Geilenkirchen. During the following years I completed my Bachelor's degree in Biomedical Engineering at the FH Aachen. My passion lies in creating things and if you want to see it for yourself, I invite you to look further into my projects bellow!",
-  resume: 'https://github.com/AndyDunkelHell/Portfolio/blob/main/Curriculum%20Andres%20Gonzalez%202023_DE.pdf',
+  resume: 'https://github.com/AndyDunkelHell/Portfolio/blob/main/CurriculumAndresGonzalez2023_ENG.pdf',
   social: {
     linkedin: ' https://www.linkedin.com/in/andres-gonzalez-rivera-13b168139/',
     github: 'https://github.com/AndyDunkelHell',
